48a848bfe0e3e065a6f911081406bf784c314395
Gendarxml <nurinah904@gmail.com>
2024-12-18 11:35:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -321,7 +321,7 @@ async function getAllConfigVless(env, hostName, listProxy) {
               .join("")}
           </div>
           <div class="tab-content">${listProxyElements}</div>
-          <a href="https://t.me/Noir7R" class="watermark" target="_blank">@Noir7R</a>
+          <a href="https://t.me/Noir7R" class="watermark" target="_blank"></a>
         </div>
         <script>
           function filterTabs() {
