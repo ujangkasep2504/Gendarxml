ef0a8b67dc44c23f77d429540276bdc5766ca1b2
Gendarxml <nurinah904@gmail.com>
2024-12-19 23:49:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from "cloudflare:sockets";
 
 var listProxy = [
   { path: "/MALASIA", proxy: "45.195.69.98:30726" },
-  { path: "/INDONESIA", proxy: "103.133.223.52,2096" },
+  { path: "/INDONESIA", proxy: "103.133.223.52:2096" },
   { path: "/SINGAPORE", proxy: "143.198.213.197:8443" },
   // tambahkan sendiri
 ];
