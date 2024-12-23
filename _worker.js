7659831da7b1c430bcb51815b099be5e02997b82
Gendarxml <nurinah904@gmail.com>
2024-12-24 02:33:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
-let proxyIPs = ['45.60.186.91:587'];
+let proxyIPs = ['103.6.207.108:8080'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 let proxyPort = proxyIP.includes(':') ? proxyIP.split(':')[1] : '443';
 
